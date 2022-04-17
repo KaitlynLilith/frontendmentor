9d99a3f51885d1e0b42ff850ef370eb12a12c4a0
KaitlynLilith <Oldsmobile86_307@yahoo.com>
2022-04-17 01:41:56 -0500

    Update faq-accordion-card-main
    Still a few bugs

--- faq-accordion-card-main/scripts/index.js
+++ faq-accordion-card-main/scripts/index.js
@@ -1,140 +1,45 @@
-// document.getElementsByClassName('#demo').addEventListener('click', third);
+// Function to show and hide faq answers
+// Receives a number placed in the onclick
+function showFAQ(num) {
 
-var arrows = document.getElementsByClassName("arrow");
-for (var i = 0; i < arrows.length; i++) {
-    arrows[i].addEventListener("click", function() {
-        if (this.closest("article > div").classList.contains("active")) {
-            this.closest("article > div").classList.remove("active");
-        } else {
-            this.closest("article > div").classList.add("active");
+    // Returns an array of all elements that could be changed when clicked
+    var reactions = document.querySelectorAll(".reaction");
+  
+    // The element named  'a' + num + 'a'
+    // One of the arrows
+    var anuma = document.getElementById("a" + num + "a");
+    
+    // The element named  'a' + num + 'b'
+    // One of the answer boxes
+    var anumb = document.getElementById("a" + num + "b");
+   
+    // The element named  'q' + num + 'h2'
+    // One of the question
+    var qnumh2 = document.getElementById("q" + num + "h2");
+
+    // If the arrow being clicked contains a class named show
+    if (anumb.classList.contains('show')) {
+
+        // Loop through everything that could be changed
+        for(let i = 0; i < reactions.length; i++) {
+            // Take .show off everything
+            reactions[i].classList.remove("show");
         }
-    });
-}
+    } else {
 
+        // Loop through everything that could be changed
+        for (let i = 0; i < reactions.length; i++) {
+            // Take .show off everything
+            reactions[i].classList.remove("show");
+        }
 
+        /* Add show to the arrow that was clicked */
+        anuma.classList.add("show");
 
+        /* Add show to the answer box that was clicked */
+        anumb.classList.add("show");
 
-
-
-// Variables for the button states
-// False means they are not clicked
-// var a1c = false;
-// var a2c = false;
-// var a3c = false;
-// var a4c = false;
-// var a5c = false;
-
-// 
-
-
-function showFAQ(bid) {
-    // if (bid.closest(".faqbox").classList.contains("active")) {
-    //     bid.closest(".faqbox").classList.remove("active");
-    // } else {
-    //     bid.closest(".faqbox").classList.add("active");
-    // }
-
-    // var current = document.getElementsByClassName("active");
-    // current[0].className = current[0].className.replace(" active", "");
-    // this.className += " active";
-
-    // var activeEl = document.activeElement.id.toString();
-    // console.log(activeEl);
-//     // Turn all the arrows down
-//     document.getElementByClassName("arrow").style.transform = "scaleY(1)";
-
-//     // Make all answers invisible
-//     getElementByClassName("answer").style.display = "none";
-
-// // Toggle by class name, value="active", in CSS
-    // switch (bid) {
-
-    //     case 'a1': 
-    //         document.getElementById("faqbox1").toggleClass('active');
-//             if (a1c === true) {
-//                 // Make a1c false
-//                 a1c = false;
-//                 return;
-//             } else {
-//                 // Turn a1 the arrow up
-//                 document.getElementById("a1a").style.transform = "scaleY(-1)";
-//                 // Make a1b answer visible
-//                 getElementById("a1b").style.display = "block";
-//                 a1c = true;
-//                 return;
-//             }
-        //     break;
-        
-        // case 'a2': 
-        //     document.getElementById("faqbox2").toggleClass('active');
-//             if (a2c === true) {
-//                 // Make a2c false
-//                 a2c = false;
-//                 return;
-//             } else {
-//                 // Turn a2 the arrow up
-//                 document.getElementById("a2a").style.transform = "scaleY(-1)";
-//                 // Make a2b answer visible
-//                 getElementById("a2b").style.display = "block";
-//                 a2c = true;
-//                 return;
-//             }
-        //     break;
-
-        // case 'a3':
-        //     document.getElementById("faqbox3").toggleClass('active'); 
-//             if (a3c === true) {
-//                 // Make a3c false
-//                 a3c = false;
-//                 return;
-//             } else {
-//                 // Turn a3 the arrow up
-//                 document.getElementById("a3a").style.transform = "scaleY(-1)";
-//                 // Make a3b answer visible
-//                 getElementById("a3b").style.display = "block";
-//                 a3c = true;
-//                 return;
-//             }
-        //     break;
-
-        // case 'a4':
-        //     document.getElementById("faqbox4").toggleClass('active'); 
-//             if (a4c === true) {
-//                 // Make a4c false
-//                 a4c = false;
-//                 return;
-//             } else {
-//                 // Turn a4 the arrow up
-//                 document.getElementById("a4a").style.transform = "scaleY(-1)";
-//                 // Make a4b answer visible
-//                 getElementById("a4b").style.display = "block";
-//                 a4c = true;
-//                 return;
-//             }
-        //     break;
-
-        // case 'a5': 
-        //     document.getElementById("faqbox5").toggleClass('active');
-//             if (a5c === true) {
-//                 // Make a5c false
-//                 a5c = false;
-//                 return;
-//             } else {
-//                 // Turn a5 the arrow up
-//                 document.getElementById("a5a").style.transform = "scaleY(-1)";
-//                 // Make a5b answer visible
-//                 getElementById("a5b").style.display = "block";
-//                 a5c = true;
-//                 return;
-//             }
-            // break;
-
-//             default:
-//                 // Turn all the arrows up
-//                 document.getElementByClassName("arrow").style.transform = "scaleY(-1)";
-
-//                 // Make all answers visible
-//                 getElementByClassName("answer").style.display = "block";
-
-    //  }
+        /* Add show to the question that was clicked  */
+        qnumh2.classList.add("show");
+    }
 }
