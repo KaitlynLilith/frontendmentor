7046ea5996bc83fa3974184fb58c5adc83b1cb80
KaitlynLilith <Oldsmobile86_307@yahoo.com>
2022-04-29 10:11:08 -0500

    Finished intro-component-with-signup-form-master
    Fixed a bug in the js

--- README.md
+++ README.md
@@ -51,6 +51,6 @@ X https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT
 
 X https://www.frontendmentor.io/challenges/base-apparel-coming-soon-page-5d46b47f8db8a7063f9331a0
 
- https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1
+X https://www.frontendmentor.io/challenges/intro-component-with-signup-form-5cf91bd49edda32581d28fd1
 
  https://www.frontendmentor.io/challenges/ping-single-column-coming-soon-page-5cadd051fec04111f7b848da
--- intro-component-with-signup-form-master/scripts/index.js
+++ intro-component-with-signup-form-master/scripts/index.js
@@ -1,49 +1,54 @@
+// Get array of the elements with class='input'
+var input = document.querySelectorAll('.input');
 
+// For each element in the input array
+for (let i = 0; i < input.length; i++) {
+    // Add an event listener that adds class='focus' to the parent element when it is onfocus
+    input[i].addEventListener("focus", ()=>{input[i].parentNode.classList.add('focus');});
 
+    // Add an event listener that removes class='focus' from the parent element when it is onblur
+    input[i].addEventListener("blur", ()=>{input[i].parentNode.classList.remove('focus');});
+}
+
+// Validates the user input in the sign-up form
+// Activates when #submitbtn is clicked
 function validation() {
     // Regex for email test
     var eval = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
+
     // Holds the first name entered by the user
     var firstName = document.getElementById('firstName').value;
 
-    console.log(firstName);
-
     // Array that holds all of the target1 class elements
+    // These elements are affected by the firstName validation
     var target1 = document.querySelectorAll('.target1')
 
-    console.log(target1);
 
     // Holds the last name entered by the user
     var lastName = document.getElementById('lastName').value;
 
-    console.log(lastName);
-
     // Array that holds all of the target2 class elements
+    // These elements are affected by the lastName validation
     var target2 = document.querySelectorAll('.target2')
 
-    console.log(target2);
 
     // Holds the email address entered by the user
     var email = document.getElementById('email').value;
 
-    console.log(email);
-
     // Array that holds all of the target3 class elements
+    // These elements are affected by the email validation
     var target3 = document.querySelectorAll('.target3')
 
-    console.log(target3);
 
     // Holds the password entered by the user
     var password = document.getElementById('password').value;
 
-    console.log(password);
-
     // Array that holds all of the target4 class elements
+    // These elements are affected by the password validation
     var target4 = document.querySelectorAll('.target4')
 
-    console.log(target4);
-
+    
     // If firstName is empty
     if (firstName === '') {
         // For every element in the target array
@@ -93,7 +98,7 @@ function validation() {
     }
 
     // If password is empty
-    if (firstName === '') {
+    if (password === '') {
         // For every element in the target array
         for (let i = 0; i < target4.length; i++) {
             // Add all error notifications
