54896530652350a12270900e95986a3f5a03355e
KaitlynLilith <Oldsmobile86_307@yahoo.com>
2022-05-19 17:06:27 -0500

    Finished time-tracking-dashboard-main

--- interactive-rating-component-main/scripts/index.js
+++ interactive-rating-component-main/scripts/index.js
@@ -1,21 +1,21 @@
-// Retrieves the value of the checked radio button, outputs it to the rating element, and changes the display values for two elements 
+// Retrieves the value of the checked radio button, outputs it to the rating element, and changes the display values for two elements
 function submitRating() {
-    
-    // Get the values from the radio buttons
-    var radios = document.getElementsByName('rateradio');
+  // Get the values from the radio buttons
+  var radios = document.getElementsByName("rateradio");
+  console.log(radios);
 
-    // Loop through the radios object
-    for (var radio of radios) {
-        // If one is checked
-        if (radio.checked) {
-            // Assign the chosen value to the rating element
-            document.getElementById('rating').innerHTML = radio.value;
-        }
+  // Loop through the radios object
+  for (var radio of radios) {
+    // If one is checked
+    if (radio.checked) {
+      // Assign the chosen value to the rating element
+      document.getElementById("rating").innerHTML = radio.value;
     }
+  }
 
-    // Make startbox invisible
-    document.getElementById("startbox").style.display = "none"; 
+  // Make startbox invisible
+  document.getElementById("startbox").style.display = "none";
 
-    // Make endbox visible
-    document.getElementById("endbox").style.display = "block"; 
+  // Make endbox visible
+  document.getElementById("endbox").style.display = "block";
 }
